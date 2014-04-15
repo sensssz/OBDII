26a58806cb16dc9b6c3bde872e8645d67bef4a8e
rogers140 <rogers140@gmail.com>
2014-04-15 20:41:54 +0800

    add drawers

--- OBDII/src/main/java/cn/edu/nju/software/obdii/ui/MainViewActivity.java
+++ OBDII/src/main/java/cn/edu/nju/software/obdii/ui/MainViewActivity.java
@@ -1,8 +1,141 @@
 package cn.edu.nju.software.obdii.ui;
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import cn.edu.nju.software.obdii.R;
+
 /**
  * Created by rogers on 4/15/14.
  */
 public class MainViewActivity extends Activity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private int[] mDrawerIcons;
+    private String[] mDrawerOptions;
+    private ActionBarDrawerToggle mDrawerToggle;
 
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainview);
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView)findViewById(R.id.left_drawer);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        mDrawerIcons = new int[] { //set icon of each drawer item
+                R.drawable.ic_launcher,
+                R.drawable.ic_launcher,
+                R.drawable.ic_launcher,
+                R.drawable.ic_launcher,
+                R.drawable.ic_launcher};
+        mDrawerOptions = new String[] { //set title of each drawer item
+                getString(R.string.car_route),
+                getString(R.string.OBD_data),
+                getString(R.string.travel_info),
+                getString(R.string.statistics),
+                getString(R.string.check_alert),
+        };
+        mDrawerList.setAdapter(new MyAdapter(
+                getActionBar().getThemedContext(), R.layout.fragment_drawer_item,
+                mDrawerIcons, mDrawerOptions));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.apptheme_ic_navigation_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+
+
+    }
+
+    public class MyAdapter extends ArrayAdapter<String> {
+        private int[] mImgs;
+        private String[] mTexts;
+        private int mViewSourceId;
+        private LayoutInflater mInflater;
+        public MyAdapter(Context context, int viewResourceId, int[] imgs, String[] texts) {
+            super(context, viewResourceId, texts);
+            mImgs = imgs;
+            mTexts = texts;
+            mViewSourceId = viewResourceId;
+            mInflater = (LayoutInflater)context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            convertView = mInflater.inflate(mViewSourceId, null);
+            ImageView imageView = (ImageView)convertView.findViewById(R.id.option_icon);
+            imageView.setImageResource(mImgs[position]);
+            TextView textView = (TextView)convertView.findViewById(R.id.option_text);
+            textView.setText(mTexts[position]);
+            return convertView;
+        }
+    }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        }
+    }
+
+    private void selectItem(int position) {
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mDrawerOptions[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 }
