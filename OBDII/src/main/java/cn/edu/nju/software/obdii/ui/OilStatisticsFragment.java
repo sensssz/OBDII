cc13bc2a16adb1ce2f5424eb8ca55318b6490e59
rogers140 <rogers140@gmail.com>
2014-04-18 12:12:29 +0800

    add test graph

--- OBDII/src/main/java/cn/edu/nju/software/obdii/ui/OilStatisticsFragment.java
+++ OBDII/src/main/java/cn/edu/nju/software/obdii/ui/OilStatisticsFragment.java
@@ -6,18 +6,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-import com.shinobicontrols.charts.ChartFragment;
-import com.shinobicontrols.charts.DataPoint;
-import com.shinobicontrols.charts.LineSeries;
-import com.shinobicontrols.charts.LineSeriesStyle;
-import com.shinobicontrols.charts.NumberAxis;
-import com.shinobicontrols.charts.NumberRange;
-import com.shinobicontrols.charts.SeriesStyle;
-import com.shinobicontrols.charts.ShinobiChart;
-import com.shinobicontrols.charts.SimpleDataAdapter;
 
-import java.util.ArrayList;
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 
 import cn.edu.nju.software.obdii.R;
 
@@ -25,54 +23,62 @@ import cn.edu.nju.software.obdii.R;
  * Created by rogers on 2014/4/17.
  */
 public class OilStatisticsFragment extends Fragment {
-    ArrayList<Double> mOilArray; //耗油量数据
-    ChartFragment mOilChart;
-    SimpleDataAdapter<Double, Double> mDataAdapter;
+
+    private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
+    private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+    private XYSeries mCurrentSeries;
+    private XYSeriesRenderer mCurrentRenderer;
+    private GraphicalView mChartView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_oil, container, false);
+
         if (savedInstanceState == null) {
-            mOilChart = (ChartFragment) getFragmentManager().findFragmentById(R.id.oil_chart);
-            ShinobiChart shinobiChart = mOilChart.getShinobiChart();
-            //set license key
-            shinobiChart.setLicenseKey("FLd6X5Ib2fgqCOLMjAxNDA1MTdyb2dlcnMxNDBAZ21haWwuY29tXo4bH54Qev+tVhnCUvhcWYQNoETk/IApw01C69+NFglOQNhHti3bUi7SzciaQSNQELzmTqZJShEUGlAvQXh+Pjy1kipnfUQF6c45I1EiQPYHvfctG8dyHMwSZqa0N27AEcEMGnsWP56NL5pd7ix8BpTa85Ec=BQxSUisl3BaWf/7myRmmlIjRnMU2cA7q+/03ZX9wdj30RzapYANf51ee3Pi8m2rVW6aD7t6Hi4Qy5vv9xpaQYXF5T7XzsafhzS3hbBokp36BoJZg8IrceBj742nQajYyV7trx5GIw9jy/V6r0bvctKYwTim7Kzq+YPWGMtqtQoU=PFJTQUtleVZhbHVlPjxNb2R1bHVzPnh6YlRrc2dYWWJvQUh5VGR6dkNzQXUrUVAxQnM5b2VrZUxxZVdacnRFbUx3OHZlWStBK3pteXg4NGpJbFkzT2hGdlNYbHZDSjlKVGZQTTF4S2ZweWZBVXBGeXgxRnVBMThOcDNETUxXR1JJbTJ6WXA3a1YyMEdYZGU3RnJyTHZjdGhIbW1BZ21PTTdwMFBsNWlSKzNVMDg5M1N4b2hCZlJ5RHdEeE9vdDNlMD08L01vZHVsdXM+PEV4cG9uZW50PkFRQUI8L0V4cG9uZW50PjwvUlNBS2V5VmFsdWU+");
+            LinearLayout layout = (LinearLayout) view.findViewById(R.id.oil_chart);
 
-            shinobiChart.setTitle(getString(R.string.oil_consume_average));
-            mDataAdapter = new SimpleDataAdapter<Double, Double>();
-            //set axis
-            NumberAxis xAxis = new NumberAxis();
-            NumberAxis yAxis = new NumberAxis();
-            xAxis.setDefaultRange(new NumberRange(0.0, 30.0));
-            shinobiChart.setXAxis(xAxis);
-            shinobiChart.setYAxis(yAxis);
 
-            refreshData();
+            // set some properties on the main renderer
+            mRenderer.setApplyBackgroundColor(true);
+            mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
+            mRenderer.setAxisTitleTextSize(16);
+            mRenderer.setChartTitleTextSize(20);
+            mRenderer.setLabelsTextSize(15);
+            mRenderer.setLegendTextSize(15);
+            mRenderer.setMargins(new int[] { 20, 30, 15, 0 });
+            mRenderer.setZoomButtonsVisible(true);
+            mRenderer.setPointSize(5);
 
-            LineSeries series = new LineSeries();
-            series.setDataAdapter(mDataAdapter);
-            shinobiChart.addSeries(series);
+            mCurrentSeries = new XYSeries(getString(R.string.oil_consume_average));
+            mDataset.addSeries(mCurrentSeries);
+            mCurrentRenderer = new XYSeriesRenderer();
+            mRenderer.addSeriesRenderer(mCurrentRenderer);
 
-            //set graph style
-            LineSeriesStyle style1 = series.getStyle();
-            style1.setFillStyle(SeriesStyle.FillStyle.GRADIENT);
-            style1.setAreaColor(Color.argb(179, 94, 51, 95));
-            style1.setAreaColorGradient(Color.argb(255,  94,  51,  95));
-            style1.setAreaColorBelowBaseline(Color.argb(179,  94,  51,  95));
-            style1.setAreaColorGradientBelowBaseline(Color.argb(255,  94,  51,  95));
+            mCurrentRenderer.setPointStyle(PointStyle.CIRCLE);
+            mCurrentRenderer.setFillPoints(true);
+            mCurrentRenderer.setDisplayChartValues(true);
+            mCurrentRenderer.setDisplayChartValuesDistance(10);
+
+            mCurrentSeries.add(1, 20);
+            mCurrentSeries.add(2, 13);
+            mCurrentSeries.add(3, 14);
+            mCurrentSeries.add(4, 21);
+            mCurrentSeries.add(5, 10);
+            mCurrentSeries.add(6, 11);
+            mCurrentSeries.add(7, 16);
+
+            mChartView = ChartFactory.getLineChartView(getActivity(), mDataset, mRenderer);
+            layout.addView(mChartView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+
 
         }
         return view;
     }
 
     private void refreshData() {
-        //test
-        for ( int i=0 ; i<100 ; i++) {
-            double radians = i * Math.PI / 25.0;
-            mDataAdapter.add(new DataPoint<Double, Double>(radians, Math.sin(radians)));
-        }
-        //
+
     }
 
 }
