3372d6c59c55d017feaa6938c0a4dea65c988be4
rogers140 <rogers140@gmail.com>
2014-04-20 11:49:52 +0800

    improve charts

--- OBDII/src/main/java/cn/edu/nju/software/obdii/ui/OilStatisticsFragment.java
+++ OBDII/src/main/java/cn/edu/nju/software/obdii/ui/OilStatisticsFragment.java
@@ -1,7 +1,6 @@
 package cn.edu.nju.software.obdii.ui;
 
 import android.app.Fragment;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -38,59 +37,65 @@ public class OilStatisticsFragment extends Fragment {
         if (savedInstanceState == null) {
             LinearLayout layout = (LinearLayout) view.findViewById(R.id.oil_chart);
 
+            mCurrentRenderer = new XYSeriesRenderer();
+            mRenderer.addSeriesRenderer(mCurrentRenderer);
+            setRendererStyle();
 
-            // set some properties on the main renderer
-            mRenderer.setApplyBackgroundColor(true);
-            mRenderer.setBackgroundColor(getResources().getColor(R.color.char_background));
-            mRenderer.setAxisTitleTextSize(20);
-            mRenderer.setAxesColor(getResources().getColor(R.color.axis_color));
-
+            //set title for axis
             mRenderer.setXTitle(getString(R.string.oil_x_label));
             mRenderer.setYTitle(getString(R.string.oil_y_label));
+
+            //set limit for axis
             mRenderer.setXAxisMax(10);
             mRenderer.setXAxisMin(0);
             mRenderer.setYAxisMax(25);
             mRenderer.setYAxisMin(0);
-            mRenderer.setMarginsColor(getResources().getColor(R.color.background_color));
-            mRenderer.setXLabelsColor(getResources().getColor(R.color.axis_color));
-            mRenderer.setYLabelsColor(0, getResources().getColor(R.color.axis_color));
 
-
-            mRenderer.setChartTitleTextSize(30);
-            mRenderer.setLabelsTextSize(30);
-            mRenderer.setLabelsColor(getResources().getColor(R.color.axis_color));
-            mRenderer.setLegendTextSize(20);
-            mRenderer.setMargins(new int[] { 100, 60, 100, 60});
-            mRenderer.setZoomButtonsVisible(false);
-            mRenderer.setPointSize(5);
-
+            //add line series
             mCurrentSeries = new XYSeries(getString(R.string.oil_consume_average));
-
             mDataset.addSeries(mCurrentSeries);
 
-            mCurrentRenderer = new XYSeriesRenderer();
-            mRenderer.addSeriesRenderer(mCurrentRenderer);
-
-            mCurrentRenderer.setPointStyle(PointStyle.CIRCLE);
-            mCurrentRenderer.setFillPoints(true);
-            mCurrentRenderer.setDisplayChartValues(true);
-            mCurrentRenderer.setColor(getResources().getColor(R.color.series_color));
-            mCurrentRenderer.setLineWidth(5);
-            mCurrentRenderer.setChartValuesTextSize(15);
-            mCurrentRenderer.setDisplayChartValuesDistance(10);
-
-            refreshData();
+            loadData();
 
             mChartView = ChartFactory.getLineChartView(getActivity(), mDataset, mRenderer);
             layout.addView(mChartView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT));
-
-
         }
         return view;
     }
 
-    private void refreshData() {
+    private void setRendererStyle() {
+        // set some properties on the main renderer
+        mRenderer.setApplyBackgroundColor(true);
+        mRenderer.setBackgroundColor(getResources().getColor(R.color.char_background));
+        mRenderer.setAxisTitleTextSize(20);
+        mRenderer.setAxesColor(getResources().getColor(R.color.axis_color));
+
+        mRenderer.setMarginsColor(getResources().getColor(R.color.background_color));
+        mRenderer.setXLabelsColor(getResources().getColor(R.color.axis_color));
+        mRenderer.setYLabelsColor(0, getResources().getColor(R.color.axis_color));
+
+        mRenderer.setChartTitleTextSize(30);
+        mRenderer.setLabelsTextSize(30);
+        mRenderer.setLabelsColor(getResources().getColor(R.color.axis_color));
+        mRenderer.setLegendTextSize(20);
+        mRenderer.setMargins(new int[] { 100, 60, 100, 60});
+        mRenderer.setZoomButtonsVisible(false);
+        mRenderer.setPointSize(5);
+
+        // set some properties on the current renderer
+        mCurrentRenderer.setPointStyle(PointStyle.CIRCLE);
+        mCurrentRenderer.setFillPoints(true);
+        mCurrentRenderer.setDisplayChartValues(true);
+        mCurrentRenderer.setColor(getResources().getColor(R.color.oil_series_color));
+        mCurrentRenderer.setLineWidth(5);
+        mCurrentRenderer.setChartValuesTextSize(20);
+        mCurrentRenderer.setDisplayChartValuesDistance(10);
+    }
+
+
+    protected void loadData() {
+        //should read file to load history data
         mCurrentSeries.add(1, 20);
         mCurrentSeries.add(2, 13);
         mCurrentSeries.add(3, 14);
@@ -98,6 +103,11 @@ public class OilStatisticsFragment extends Fragment {
         mCurrentSeries.add(5, 10);
         mCurrentSeries.add(6, 11);
         mCurrentSeries.add(7, 16);
+    }
+
+    protected void addData(int times, double data) {
+        mCurrentSeries.add(times, data);
+        mChartView.repaint();
     }
 
 }
